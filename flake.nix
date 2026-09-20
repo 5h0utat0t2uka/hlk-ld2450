91f42863310631fd40a212ff26657aab43f63b06
5h0utat0t2uka <37361670+5h0utat0t2uka@users.noreply.github.com>
2026-09-20 15:45:09 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Arduino development environment for M5Stack CoreS3 and HLK-LD2450";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = [ pkgs.arduino-cli ];
+      };
+    };
+}
